38e5eb0bb2c834259a0f21a99941c1307c3c0232
heena meena <heena meena>
2016-11-25 23:39:51 +0530

    question bar removed ,reg added but ngshow not working

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -16,10 +16,21 @@ angular.module('starter.controllers', [])
 })
 
 
-.controller('DashCtrl', function($scope,$state,$rootScope) {
 
+
+.controller('DashCtrl', function($scope,$state) {
+
+	$scope.valuefront = true;
+	$scope.value = false;
 	$scope.votes = 0;
 	$scope.downvotes = 0;
+	$state.reguser = function(){
+		 $scope.$apply(function(){
+            $scope.valuefront = false;
+			$scope.value = true;
+        });
+		$state.go('tab.OneAns');
+	}
 	$scope.upVote = function(s){
 		s.votes++;
 	}
@@ -170,10 +181,10 @@ angular.module('starter.controllers', [])
 	
 	
 })
+
 .controller('AccountCtrl', function($scope) {
   $scope.settings = {
     enableFriends: true
   };
-
 
 });
